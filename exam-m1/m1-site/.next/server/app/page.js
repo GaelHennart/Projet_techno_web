c78ee6c656647b0aea96be50fd0cda4c508ce3dc
Matheo <matheo.stephan@student.junia.com>
2024-11-14 20:45:36 +0100

    modif

--- exam-m1/m1-site/.next/server/app/page.js
+++ exam-m1/m1-site/.next/server/app/page.js
@@ -249,4 +249,4 @@ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s 
 var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/@swc","vendor-chunks/tailwindcss"], () => (__webpack_exec__("(rsc)/./node_modules/next/dist/build/webpack/loaders/next-app-loader/index.js?name=app%2Fpage&page=%2Fpage&appPaths=%2Fpage&pagePath=private-next-app-dir%2Fpage.tsx&appDir=C%3A%5CUsers%5Cmstep%5CProjet_techno_web-4%5Cexam-m1%5Cm1-site%5Csrc%5Capp&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&rootDir=C%3A%5CUsers%5Cmstep%5CProjet_techno_web-4%5Cexam-m1%5Cm1-site&isDev=true&tsconfigPath=tsconfig.json&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=&middlewareConfig=e30%3D!")));
 module.exports = __webpack_exports__;
 
-})();                                                 
+})();
